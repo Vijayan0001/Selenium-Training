6f526887557657d7dd024115da9f7f9c17c46140
Vijayan0001 <born2win9186@gmail.com>
2024-09-11 00:53:00 -0700

    Updated Code

--- src/org/selenium/training/SeleniumProject.java
+++ src/org/selenium/training/SeleniumProject.java
@@ -1,5 +1,41 @@
 package org.selenium.training;
 
-public class SeleniumProject {
+import java.time.Duration;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumProject {
+	
+ public static void main(String[] args) {
+	 
+	WebDriver driver= new ChromeDriver();
+	
+	driver.get("https://www.facebook.com/");
+	
+	 driver.manage().window().maximize();
+	 
+	 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	 
+	 WebElement txtUser = driver.findElement(By.id("email"));
+	 txtUser.sendKeys("Vijayan");
+	 
+	 WebElement txtPass = driver.findElement(By.id("pass"));
+	 txtPass.sendKeys("8765234");
+	 
+	 WebElement btnLogin = driver.findElement(By.name("login"));
+	 btnLogin.click();
+	 
+	 WebElement signUp = driver.findElement(By.xpath(("//a[text()='Sign up for Facebook']")));
+	 signUp.click();
+	 
+	 
+	 
+	 
+	 
+	 
+	
+}
 }
